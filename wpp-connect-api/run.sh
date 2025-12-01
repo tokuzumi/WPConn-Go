4e11ebb7715ea0aaddba2950c21990affa7d20ff
tokuzumi <daniel.tokuzumi@gmail.com>
2025-12-01 06:15:10 -0300

    Fix mixed content: enable proxy headers in Uvicorn

--- wpp-connect-api/run.sh
+++ wpp-connect-api/run.sh
@@ -6,4 +6,4 @@ alembic upgrade head
 
 # Inicia a aplicação
 echo "Starting application..."
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips '*'
